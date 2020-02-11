cee4fda2930ff366df97623236ed2b09b5654e54
Daniel <d.olugbade@outlook.com>
2020-02-11 23:49:14 +0000

    Connected frontend to backend, adjusted config and dependencies

--- client/src/app/components/Home.tsx
+++ client/src/app/components/Home.tsx
@@ -1,12 +1,27 @@
 import * as React from "react";
 
-export interface IHomeProps { 
-    firstName: string, 
-    lastName: string
+type HomeProps = {
+    firstName: string,
+    lastName: string,
 }
 
-export const Home: React.FC<IHomeProps> = (props)  => {
+export const Home: React.FC<HomeProps> = (props) => {
+
+    let [todos, setTodos] = React.useState([]);
+
+    React.useEffect(() => {
+        async function fetchData() {
+            const response = await fetch(`http://localhost:2700/todos`);
+            const responseData = await response.json();
+            setTodos(responseData);
+        }
+        fetchData();
+    }, []);
+
     return (
-        <h1>Hello {props.firstName} {props.lastName}</h1>
+        <div>
+            <h1>Hello {props.firstName} {props.lastName}</h1>
+            <button className="btn btn-primary" onClick={() => { console.log(todos) }}>Log Data</button>
+        </div>
     );
 };
