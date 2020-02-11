721afeb9c5de56e52a84e9d66f939a50a0c86388
Daniel <d.olugbade@outlook.com>
2020-02-11 22:44:27 +0000

    Created endpoints for TODO CRUD functionality

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -12,4 +12,58 @@ router.get('', async (req, res) => {
     }
 });
 
+router.post('', async (req, res) => {
+    try {
+        let first = null;
+        first = await models.Todo.create({ ...req.body, date: new Date() });
+        if (first) {
+            res.send({ message: 'Added Todo' });
+        }
+        else {
+            res.status(404).send({ 'message': 'Todo not found' });
+        }
+    } catch (exc) {
+        console.error(exc);
+    }
+});
+
+router.put('', async (req, res) => {
+    try {
+        let first = null;
+        const { name, description } = req.body;
+        first = await models.Todo.update({ name: name, description: description   }, {
+            where: {
+                id: req.body.id
+            }
+        });
+        if (first) {
+            res.send({ message: 'Updated Todo with ID: ' + req.body.id });
+        }
+        else {
+            res.status(404).send({ 'message': 'Failed to update Todo with ID ' + req.body.id });
+        }
+    } catch (exc) {
+        console.error(exc);
+    }
+});
+
+router.delete('', async (req, res) => {
+    try {
+        let first = null;
+        first = await models.Todo.destroy({
+            where: {
+                id: req.body.id
+            }
+        });
+        if (first) {
+            res.send({ message: 'Deleted Todo with ID: ' + req.body.id });
+        }
+        else {
+            res.status(404).send({ 'message': 'Failed to delete Todo with ID ' + req.body.id });
+        }
+    } catch (exc) {
+        console.error(exc);
+    }
+});
+
 module.exports = router;
