8c36ca502575b31bae8d48d7c54faca0eeff29de
Daniel <d.olugbade@outlook.com>
2020-02-24 13:20:53 +0000

    Enabled initialisation of tables

--- server/models/index.js
+++ server/models/index.js
@@ -15,13 +15,13 @@ const Todo = sequelize.import(__dirname + '/todo-model');
 const Message = sequelize.import(__dirname + '/message-model');
 
 //I used these methods once so that sequelize would initialize the new tables with data.
-// sequelize.sync({ force: true }).then(() => {
-//     Todo.create({ name: 'Get Files', description: 'By Friday', date: new Date()});
-// });
+sequelize.sync({ force: true }).then(() => {
+    Todo.create({ name: 'Get Files', description: 'By Friday', date: new Date()});
+});
 
-// sequelize.sync({ force: true }).then(() => {
-//     Message.create({ message: 'First'});
-// });
+sequelize.sync({ force: true }).then(() => {
+    Message.create({ message: 'First'});
+});
 
 module.exports =  {
     Todo,
